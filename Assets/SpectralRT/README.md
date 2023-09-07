992a22edf5202187436a29bea6fc0b672a6ce899
Luke Siemens <luke@lsiemens.com>
2023-09-07 11:26:05 -0700

    Start working on ray traced spectrum code

---
+++ Assets/SpectralRT/README.md
@@ -0,0 +1,26 @@
+# Spectral
+Initial attempt to ray trace light spectrum. All spectral quantities (emission
+spectrum, material properties, imaging filters ...) will be approximated as the
+sum of Gaussian functions. Then the Gaussians can be represented by a 3D vector,
+where the product of Gaussians is the sum of the vector representations and the
+integral of the function can be computed analytically from the terms. However there
+are two major problems. First, the sum of Gaussians is not, generally, a Gaussian
+this could be solved by keeping an array of 3D vectors representing the sum of the
+terms, however this leads into the next problem. Second, assuming each function
+is approximated as the sum m Gaussians, then the spectrum of a ray bouncing off
+of n surface will have $m^(n+2)$ terms, including the interaction of the light
+source and detector filter.
+
+These problems will be addressed by using Monte Carlo sampling of the function.
+Each ray will only track a single Gaussian term, the integral of which will be 
+accumulated to produce the final approximation.
+
+## Gaussian functions
+Each Gaussian term $f(x)$ will be represented as $f(x)=e^(-ax^2 + bx + c)$ where
+$a, b, c \in \mathbb{R}$ and $a \geq 0$. From this function we can compute some
+parameters $x_0$, $f_0$ and $I$, where $x_0 = \frac{b}{2a^2}$ is the location of
+the peak, $f_0 = e^{\frac{b^2}{4a^2} + c}$ is the value at the peak and
+$I=\int_{-\infty}^{\infty}f(x)dx = \frac{f_0 \sqrt{\pi}}{a}$ is the integral of
+the function. Using these parameters the function has an alternative form
+$f(\Delta x) = f_0 e^{-a^2\Delta x^2}$ with $\Delta x = x - x_0$.
+
---
+++ Assets/SpectralRT/SpectralRTManager.cs
@@ -0,0 +1,56 @@
+// Spectral Ray Tracing Manager
+
+// based on a tutorial by David Kuri which can be found at http://blog.three-eyed-games.com/2018/05/03/gpu-ray-tracing-in-unity-part-1/
+// this component must be attached to a camera.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class SpectralRTManager : MonoBehaviour {
+    public ComputeShader RayTracingShader;
+    public Light DirectionalLight;
+    
+    private RenderTexture _target;
+    private Camera _camera;
+    
+    private void Awake() {
+        _camera = GetComponent<Camera>();
+    }
+    
+    private void SetShaderParameters() {
+        RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+        
+        Vector3 direction = DirectionalLight.transform.forward;
+        RayTracingShader.SetVector("_DirectionalLight", new Vector4(direction.x, direction.y, direction.z, DirectionalLight.intensity));
+    }
+    
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        InitRenderTexture();
+        SetShaderParameters();
+        
+        RayTracingShader.SetTexture(0, "Result", _target);
+        
+        // group pixles into blocks as specified in the `numthreads` call before
+        // the kernel.
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+        
+        Graphics.Blit(_target, destination);
+    }
+    
+    private void InitRenderTexture() {
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height) {
+            if (_target != null) {
+                _target.Release();
+            }
+            
+            _target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+    }
+}
