8e68557c36f24820c1d8b4be979557d506237e81
Luke Siemens <luke@lsiemens.com>
2023-09-08 20:52:34 -0700

    Add an imaginary term to gaussianApprox.py to account for the sign of the function

--- Assets/SpectralRT/README.md
+++ Assets/SpectralRT/README.md
@@ -15,12 +15,47 @@ These problems will be addressed by using Monte Carlo sampling of the function.
 Each ray will only track a single Gaussian term, the integral of which will be 
 accumulated to produce the final approximation.
 
-## Gaussian functions
-Each Gaussian term $f(x)$ will be represented as $f(x)=e^{-ax^2 + bx + c}$ where
-$a, b, c \in \mathbb{R}$ and $a \geq 0$. From this function we can compute some
-parameters $x_0$, $f_0$ and $I$, where $x_0 = \frac{b}{2a}$ is the location of
-the peak, $f_0 = e^{\frac{b^2}{4a} + c}$ is the value at the peak and
-$I=\int_{-\infty}^{\infty}f(x)dx = f_0 \sqrt{\frac{\pi}{a}}$ is the integral of
-the function. Using these parameters the function has an alternative form
-$f(\Delta x) = f_0 e^{-a\Delta x^2}$ with $\Delta x = x - x_0$.
+Note, when approximating nontrivial functions Gaussians with negative amplitudes
+will be used and at some points the approximated function may be negative even if
+the original function is positive everywhere.
 
+## Gaussian Functions
+Each Gaussian term $f(x)$ will be represented as $f(x)=e^{-ax^2 + bx + c + i\pi d}$
+where $a, b, c \in \mathbb{R}$, $a \geq 0$ and $d \in \mathbb{Z}$. From this
+function we can compute some parameters $x_0$, $f_0$ and $I$, where $x_0 = \frac{b}{2a}$
+is the location of the peak, $f_0 = e^{\frac{b^2}{4a} + c + i\pi d}$ is the value
+at the peak and $I=\int_{-\infty}^{\infty}f(x)dx = f_0 \sqrt{\frac{\pi}{a}}$ is
+the integral of the function. Using these parameters the function has an alternative
+form $f(\Delta x) = f_0 e^{-a\Delta x^2}$ with $\Delta x = x - x_0$. Note d is an
+integer used to encode the sign of the Gaussian where for positive functions it is
+even and for negative functions it is odd.
+
+## Best Fit Gaussian
+The method I am using for approximating functions with Gaussians requires that
+I find the best fitting Gaussian at a point. For a function $f(x)$ and a target
+point $x_0$, first the parameter $d$ is determined from the sign of $f(x_0)$ then
+going forward negate $f(x)$ if necessary so that $f(x_0)$ is positive for the
+remaining steps. The best fitting Gaussian at $x_0$ can be found calculating the
+first three terms of the polynomial approximation of $\ln{f(x)}$.
+
+So defining $x = x_0 + \Delta x$ then
+$\ln{f(x_0 + \Delta x)} = c + b(x_0 + \Delta x) - a (x_0 + \Delta x)^2$ ignoring
+higher order terms. Repeatedly differentiating and evaluating at $\Delta x = 0$
+gives the equations.
+
+$$\ln{f(x_0)} = c + b x_0 - a x_0^2$$
+
+$$\frac{f'(x_0)}{f(x_0)} = b - 2a x_0$$
+
+$$\frac{f(x_0)f''(x_0) - f'(x_0)^2}{f(x_0)^2} = -2a$$
+
+Solving this system of equations gives the coefficients,
+
+$$a = \frac{f'(x_0)^2 - f(x_0)f''(x_0)}{2f(x_0)^2}$$
+
+$$b = x_0 \frac{f'(x_0)^2 - f(x_0)f''(x_0)}{f(x_0)^2} + \frac{f'(x_0)}{f(x_0)}$$
+
+$$c = x_0^2 \frac{f(x_0)f''(x_0) - f'(x_0)^2}{2f(x_0)^2} - x_0 \frac{f'(x_0)}{f(x_0)} + \ln{f(x_0)}$$
+
+Note that if $a < 0$, then the resulting terms do not describe a Gaussian and there
+is no best fit Gaussian at that point.
--- PythonTests/gaussian_fit/gaussianApprox.py
+++ PythonTests/gaussian_fit/gaussianApprox.py
@@ -5,8 +5,17 @@ Approximate sum of gaussians polynomial
 from matplotlib import pyplot
 import numpy
 
-def gaussian(x, a, b, c, d=1):
-    return d*numpy.exp(-a*x**2 + b*x + c)
+def gParameters(x_0, f_0, I):
+    a = (f_0/I)**2*numpy.pi
+    b = 2*a*x_0
+    c = numpy.log(numpy.abs(f_0)) - b*x_0 + a*x_0**2
+    d = numpy.real(numpy.log(numpy.abs(f_0)/f_0 + 0.0j)/(1.0j*numpy.pi))
+    return a, b, c, d
+
+def gaussian(x, a, b, c, d):
+    if not numpy.isclose(d%1.0 , 0.0):
+        raise RuntimeError(f"d must be an integer: {d}")
+    return numpy.real(numpy.exp(-a*x**2 + b*x + c + 1.0j*numpy.pi*d))
 
 def plank(x, T, mult=1):
     """
@@ -20,77 +29,72 @@ def plank(x, T, mult=1):
 def lorentz(x, x_0, width=1, mult=1):
     return mult/(1 + (2*(x - x_0)/width)**2)
 
-num = 10000
-N = 2
-x_range = (5, 2000)
-
-x = numpy.linspace(*x_range, num)
-
-fun = numpy.empty((N, num))
-
-block = 0*x
-for i in range(5):
-    block += 0.8**(i + 1)*lorentz(x, 650 + 100*0.5**i, 25*0.5**i)
-
-#fun[0] = plank(x, 5500, 1E16)
-#fun[0] = lorentz(x, 550, 50)
-#fun[0] = plank(x, 5500, 1E16)*(1 - 0.5*lorentz(x, 750, 100) - 0.25*lorentz(x, 800, 50))
-fun[0] = plank(x, 5500, 1E16)*(1 - block)
-a = []
-b = []
-c = []
-d = []
-
-def find_terms(x, func, index):
+def find_terms(x, function, index):
     x_0 = x[index]
-    f_0 = func[index]
+    f_0 = function[index]
     if (f_0 < 0):
-        d = -1
-    else:
         d = 1
+    else:
+        d = 0
 
-    fun_p = numpy.gradient(numpy.log(d*func), x)
+    fun_p = numpy.gradient(numpy.log(numpy.abs(function)), x)
     fun_p2 = numpy.gradient(fun_p, x)
 
     a = -0.5*fun_p2[index]
     if a <= 0:
         return None
     b = fun_p[index] + 2*a*x_0
-    c = numpy.log(d*f_0) - b*x_0 + a*x_0**2
-    return a, b, c, d, d*f_0*numpy.sqrt(numpy.pi/a)
+    c = numpy.log(numpy.abs(f_0)) - b*x_0 + a*x_0**2
+    d = numpy.real(numpy.log(numpy.abs(f_0)/f_0 + 0.0j)/(1.0j*numpy.pi))
+    print(a, b, c, d)
+    return a, b, c, d, f_0*numpy.sqrt(numpy.pi/a)
 
+num = 10000
+N = 2
+x_range = (5, 2000)
+
+x = numpy.linspace(*x_range, num)
+
+function = numpy.empty((N, num))
+coefficients = numpy.zeros((N, 4))
+
+block = 0*x
+for i in range(5):
+    block += 0.8**(i + 1)*lorentz(x, 650 + 100*0.5**i, 25*0.5**i)
+
+#function[0] = plank(x, 5500, 1E16)
+#function[0] = lorentz(x, 550, 50)
+#function[0] = plank(x, 5500, 1E16)*(1 - 0.5*lorentz(x, 750, 100) - 0.25*lorentz(x, 800, 50))
+function[0] = plank(x, 5500, 1E16)*(1 - block)
+
+approximation = 0*x
 for i in range(N - 1):
-    df = numpy.gradient(fun[i], x)
+    df = numpy.gradient(function[i], x)
     indices = numpy.where(df[1:]*df[:-1] <= 0)[0]
-    print(indices)
-    print(x[indices])
 
     best_solver = None
     best_I = 0
     for I in indices:
-        solver = find_terms(x, fun[i], I)
+        solver = find_terms(x, function[i], I)
         if solver is not None:
-            if solver[-1] > best_I:
+            if numpy.abs(solver[-1]) > best_I:
                 best_solver = solver
-                best_I = solver[-1]
+                best_I = numpy.abs(solver[-1])
 
     solver = best_solver
 
     if solver is None:
         print("error")
         break
-    _a, _b, _c, _d, _I = solver
-    a.append(_a)
-    b.append(_b)
-    c.append(_c)
-    d.append(_d)
 
-    approx = gaussian(x, a[i], b[i], c[i], d[i])
+    coefficients[i] = numpy.array(solver[:-1])
 
-    fun[i + 1] = fun[i] - approx
-#    print(f"Solution[a:{a[i]}, b:{b[i]}, c:{c[i]}]")
+    gaussian_term = gaussian(x, *coefficients[i])
+    approximation += gaussian_term
 
-#    pyplot.plot(x, fun[i], label="Original")
+    function[i + 1] = function[i] - gaussian_term
+
+#    pyplot.plot(x, function[i], label="Original")
 #    pyplot.plot(x, approx, label="gauss")
 #    pyplot.legend()
 
@@ -98,15 +102,26 @@ for i in range(N - 1):
 #    pyplot.show()
 
 print("Done")
-pyplot.plot(x, fun[0], label="Original")
-final = 0*x
-for i in range(N-1):
-    final += gaussian(x, a[i], b[i], c[i], d[i])
-pyplot.plot(x, final, label="gaussian sum")
+pyplot.plot(x, function[0], label="Original")
+pyplot.plot(x, approximation, label="gaussian sum")
 pyplot.legend()
 
 dx = x[1] - x[0]
-print("Error: ", numpy.sum(numpy.abs(fun[0] - final))*dx)
+print("Error: ", numpy.sum(numpy.abs(function[0] - approximation))*dx)
 pyplot.show()
 
+# test multiplication as addition of vectors
 
+g_coeff = numpy.array(gParameters(1000, -1, -500))
+pyplot.plot(x, gaussian(x, *g_coeff))
+pyplot.show()
+
+coefficients += g_coeff
+approximation = 0*x
+for i in range(N-1):
+    approximation += gaussian(x, *coefficients[i])
+
+pyplot.plot(x, function[0]*gaussian(x, *g_coeff), label="Original")
+pyplot.plot(x, approximation, label="gaussian sum")
+pyplot.legend()
+pyplot.show()
